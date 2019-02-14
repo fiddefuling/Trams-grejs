0ec4cc69ef60a7d6bd0d45bb6593365ee3770a6d
DPontes <dpontes@protonmail.com>
2019-02-14 13:08:10 +0100

    Several changes to improve readability

--- ultrasonicTime.ino
+++ ultrasonicTime.ino
@@ -5,37 +5,47 @@
   * TRIG connection of the sensor attached to digital pin 12
   * ECHO connection of the sensor attached to digital pin 13
 */
-  const int TRIG_PIN = 12;
-  const int ECHO_PIN = 13;
-  unsigned long time;
-  float timeseconds;
-  
-  void setup() {
-  // Initialize serial communication
-  Serial.begin(9600);
-  pinMode(TRIG_PIN, OUTPUT);
-  pinMode(ECHO_PIN, INPUT);
-  }
-  
-  void loop() {
-  long duration, distanceCm;
-  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
-  digitalWrite(TRIG_PIN, LOW);
-  delayMicroseconds(2);
-  digitalWrite(TRIG_PIN, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIG_PIN, LOW);
-  duration = pulseIn(ECHO_PIN, HIGH);
-  // Convert the time into a distance
-  distanceCm = duration /29.1 / 2;
-  if (distanceCm <= 200) {
-  // Serial.println("Detected!");
-  Serial.print("Time: "); 
-  time = millis();
-  timeseconds = time/1000.0;
-   
-  Serial.print(timeseconds); 
-  Serial.println(" seconds");
-  }
-  delay(500);
-  }
+const int TRIG_PIN = 12;
+const int ECHO_PIN = 13;
+unsigned long time;
+float timeseconds;
+long duration, distanceCm;
+
+
+void applyShortLowPulse() {
+    // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+    digitalWrite(TRIG_PIN, LOW);
+    delayMicroseconds(2);
+    digitalWrite(TRIG_PIN, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIG_PIN, LOW);
+}
+
+
+float timeToDistance(int duration) {
+    return duration / 29.1 / 2;
+}
+
+
+void setup() {
+    // Initialize serial communication
+    Serial.begin(9600);
+    pinMode(TRIG_PIN, OUTPUT);
+    pinMode(ECHO_PIN, INPUT);
+    applyShortLowPulse();
+}
+
+
+void loop() {
+    duration = pulseIn(ECHO_PIN, HIGH);
+    distanceCm = timeToDistance(duration);
+    if (distanceCm <= 200) {
+        Serial.print("Time: "); 
+        time = millis();
+        timeseconds = time/1000.0;
+
+        Serial.print(timeseconds); 
+        Serial.println(" seconds");
+    }
+    delay(500);
+}
